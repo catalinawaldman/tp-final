ce46ced6c5e52ec91e09648f6de615183ca2d664
catawaldman <catalinawaldman@hotmail.com>
2026-02-25 14:49:39 -0300

    fix: mensajes guardados en el localStorage

--- src/context/ChatContext.jsx
+++ src/context/ChatContext.jsx
@@ -1,56 +1,78 @@
-import { createContext, useContext, useState } from "react"
+import { createContext, useState, useEffect } from "react"
 import { users as mockUsers } from "../services/mockApi"
 
 const ChatContext = createContext()
 
 const ChatProvider = ({ children }) => {
-  const [users, setUsers] = useState(mockUsers)
+
+  const [users, setUsers] = useState(() => {
+    const storedUsers = localStorage.getItem("users")
+    return storedUsers ? JSON.parse(storedUsers) : mockUsers
+  })
+
   const [selectedUserId, setSelectedUserId] = useState(null)
-  const [loggedUser, setLoggedUser] = useState(JSON.parse(localStorage.getItem("user")) || null)
+
+  const [loggedUser, setLoggedUser] = useState(
+    JSON.parse(localStorage.getItem("user")) || null
+  )
+
+  const handleSelectedUserId = (id) => {
+    setSelectedUserId(id)
+  }
+  
+  useEffect(() => {
+    localStorage.setItem("users", JSON.stringify(users))
+  }, [users])
 
   const handleUser = (user) => {
     setLoggedUser(user)
     localStorage.setItem("user", JSON.stringify(user))
   }
 
-  const handleSelectedUserId = (id) => {
-    setSelectedUserId(id)
-  }
-const login = (userData) => {
-  const foundUser = mockUsers.find(
-    user => user.email === userData.email
-  )
+  const login = (userData) => {
+    const foundUser = users.find(
+      user => user.email === userData.email
+    )
 
-  if (!foundUser) {
+    if (!foundUser) return null
+    if (foundUser.password === userData.password) return foundUser
+
     return null
   }
 
-  if (foundUser.password === userData.password) {
-    return foundUser
-  }
-
-  return null
-}
-
   const logout = () => {
     localStorage.removeItem("user")
+    setLoggedUser(null)
   }
 
   const handleMessages = (newMessage) => {
-    setUsers((prevValue) => prevValue.map((user) =>
-      user.id === selectedUserId
-        ? {
-          ...user,
-          messages: [...user.messages, newMessage]
-        }
-        : user
-    ))
+    setUsers((prevValue) =>
+      prevValue.map((user) =>
+        user.id === selectedUserId
+          ? {
+              ...user,
+              messages: [...user.messages, newMessage]
+            }
+          : user
+      )
+    )
   }
 
   const selectedUser = users.find(user => user.id === selectedUserId)
 
   return (
-    <ChatContext.Provider value={{ users, handleSelectedUserId, login, logout, handleUser, loggedUser, handleMessages, selectedUser }}>
+    <ChatContext.Provider
+      value={{
+        users,
+        handleSelectedUserId,
+        login,
+        logout,
+        handleUser,
+        loggedUser,
+        handleMessages,
+        selectedUser
+      }}
+    >
       {children}
     </ChatContext.Provider>
   )
