be34fba8c0b5ef5dc6bf630192447ddccd0982ef
catawaldman <catalinawaldman@hotmail.com>
2026-02-25 14:16:25 -0300

    add:login base

--- src/context/ChatContext.jsx
+++ src/context/ChatContext.jsx
@@ -17,3 +17,46 @@ const ChatProvider = ({ children }) => {
     setSelectedUserId(id)
   }
 }
+
+{
+const login = (userData) => {
+  const foundUser = mockUsers.find(
+    user => user.email === userData.email
+  )
+
+  if (!foundUser) {
+    return null
+  }
+
+  if (foundUser.password === userData.password) {
+    return foundUser
+  }
+
+  return null
+}
+
+  const logout = () => {
+    localStorage.removeItem("user")
+  }
+
+  const handleMessages = (newMessage) => {
+    setUsers((prevValue) => prevValue.map((user) =>
+      user.id === selectedUserId
+        ? {
+          ...user,
+          messages: [...user.messages, newMessage]
+        }
+        : user
+    ))
+  }
+
+  const selectedUser = users.find(user => user.id === selectedUserId)
+
+  return (
+    <ChatContext.Provider value={{ users, handleSelectedUserId, login, logout, handleUser, loggedUser, handleMessages, selectedUser }}>
+      {children}
+    </ChatContext.Provider>
+  )
+}
+
+export { ChatContext, ChatProvider }
